d36fb7b8920adecdf4ef38b1864cedd34e015e54
rvestra <rvestra@unm.edu>
2020-02-13 11:34:53 -0700

    remove provider register

--- src/BusinessObjectsFinanceServiceProvider.php
+++ src/BusinessObjectsFinanceServiceProvider.php
@@ -2,12 +2,11 @@
 namespace Unmit\BusinessObjects\Finance;
 
 use Illuminate\Support\ServiceProvider;
-use Unmit\ldk\BusinessObjects\BusinessObjectsServiceProvider;
 
 class BusinessObjectsFinanceServiceProvider extends ServiceProvider {
     public function boot()
     {
-        $this->app->register(BusinessObjectsServiceProvider::class);
+
     }
     public function register()
     {
