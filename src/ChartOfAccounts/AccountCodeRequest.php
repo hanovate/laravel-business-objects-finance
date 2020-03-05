d86d67955700013c7df9706170c43c4beeaadb38
rvestra <rvestra@unm.edu>
2020-03-05 14:51:24 -0700

    added acct

--- src/ChartOfAccounts/AccountCodeRequest.php
+++ src/ChartOfAccounts/AccountCodeRequest.php
@@ -25,15 +25,19 @@ class AccountCodeRequest extends Request
         $items = $this->getFields();
 
         //Predecessor V_PRED IN VARCHAR2
-        $predecessor = new BusinessObjectItem('predecessor', 'Predecessor', 'V_PRED');
+        $predecessor = new BusinessObjectItem('predecessor', 'Predecessor', 'FZBCOAM_ACCT_PRED');
         $items->push($predecessor);
 
         // Fund Type V_FND_TYPE IN VARCHAR2 populated with Fund
-        $type = new BusinessObjectItem('type', 'Type', 'V_TYPE');
+        $type = new BusinessObjectItem('account', 'Account', 'FZBCOAM_ACCT');
+        $items->push($type);
+
+        // Fund Type V_FND_TYPE IN VARCHAR2 populated with Fund
+        $type = new BusinessObjectItem('type', 'Type', 'FZBCOAM_ACCT_TYPE');
         $items->push($type);
 
         //  Is data entry V_IS_DATAENTRY IN VARCHAR2 populated for Account, Fund, & Program
-        $isDataEntry = new BusinessObjectItem('isDataEntry', 'Is Data Entry', 'V_DATAENTRY');
+        $isDataEntry = new BusinessObjectItem('isDataEntry', 'Is Data Entry', 'FZBCOAM_ACCT_DATAENTRY');
         $items->push($isDataEntry);
 
         $this->setFields($items);
