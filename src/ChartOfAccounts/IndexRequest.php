f771221d3fb54f175f1d9eae63f2fb8842857cf7
rvestra <rvestra@unm.edu>
2020-03-27 15:03:31 -0600

    updated index with fields

--- src/ChartOfAccounts/IndexRequest.php
+++ src/ChartOfAccounts/IndexRequest.php
@@ -14,39 +14,39 @@ use Unmit\ldk\BusinessObjects\BusinessObjectItem;
  */
 class IndexRequest extends Request
 {
-    public $organizationCode;
-    public $financialManager;
-    public $fund;
-    public $program;
-    public $activity;
-
-
     public function __construct()
     {
         parent::__construct();
 
         $items = $this->getFields();
-
-        // Organization Code
-        $organizationCode = new BusinessObjectItem('organizationCode','Organization Code', 'V_ORGN');
-        $items->push($organizationCode);
-
-        //  Financial manager V_FINMAN IN VARCHAR2 populated for Index based on Fund
-        $financialManager = new BusinessObjectItem('financialManager', 'Financial Manager', 'V_FINMAN');
-        $items->push($financialManager);
-
-        // Fund   V_FUND IN VARCHAR2 new Index
-        $fund = new BusinessObjectItem('fund', 'Fund', 'V_FUND');
-        $items->push($fund);
-
-        //  Program V_PROG IN VARCHAR2 new Index
-        $program = new BusinessObjectItem('program', 'Program', 'V_PROG');
-        $items->push($program);
-
-        //  Activity V_ACTV IN VARCHAR2 new Index
-        $activity = new BusinessObjectItem('activity', 'Activity', 'V_ACTV');
-        $items->push($activity);
-
+        $elems = [
+            [
+                BusinessObjectItem::NAME => 'title',
+                BusinessObjectItem::BUSINESS_NAME => 'Title',
+                BusinessObjectItem::COLUMN_NAME => 'FZBCOAM_INDEX_TITLE'
+            ],
+            [
+                BusinessObjectItem::NAME => 'organization',
+                BusinessObjectItem::BUSINESS_NAME => 'Organization',
+                BusinessObjectItem::COLUMN_NAME => 'FZBCOAM_ORGN'
+            ],
+            [
+                BusinessObjectItem::NAME => 'fund',
+                BusinessObjectItem::BUSINESS_NAME => 'Fund',
+                BusinessObjectItem::COLUMN_NAME => 'FZBCOAM_FUND'
+            ],
+            [
+                BusinessObjectItem::NAME => 'program',
+                BusinessObjectItem::BUSINESS_NAME => 'Program',
+                BusinessObjectItem::COLUMN_NAME => 'FZBCOAM_PROG'
+            ],
+            [
+                BusinessObjectItem::NAME => 'activity',
+                BusinessObjectItem::BUSINESS_NAME => 'Activity',
+                BusinessObjectItem::COLUMN_NAME => 'FZBCOAM_ACTV'
+            ]
+        ];
+        $this->pushElements($items,$elems);
         $this->setFields($items);
     }
 
