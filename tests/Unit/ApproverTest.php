59b8f5790d1f234890a2023d8e4d85fd597383e2
rvestra <rvestra@unm.edu>
2020-04-03 14:33:44 -0600

    fixed request ref with approver

--- tests/Unit/ApproverTest.php
+++ tests/Unit/ApproverTest.php
@@ -5,26 +5,26 @@ namespace Unmit\BusinessObjects\Business\Finance\Tests\Unit;
 
 use Unmit\BusinessObjects\Business\Finance\Tests\TestCase;
 use Unmit\ldk\BusinessObjects\BusinessObjectInterface;
-use Unmit\BusinessObjects\Business\Finance\ChartOfAccounts\Request;
+use Unmit\BusinessObjects\Business\Finance\ChartOfAccounts\Approver;
 
 
-final class RequestTest extends TestCase
+final class ApproverTest extends TestCase
 {
     public function testIsInterface()
     {
-        $request = new Request();
+        $request = new Approver();
         $this->assertInstanceOf(BusinessObjectInterface::class, $request);
     }
     public function testColumnsAreAccurateTest()
     {
-        $request = new Request();
+        $request = new Approver();
         $columnList = ['APPR_PIDM','APPR_EMAIL','APPR_NAME'];
         $this->assertEqualsCanonicalizing($columnList,$request->getColumnNames());
 
     }
     public function testBusinessNamesAreAccurateTest()
     {
-        $request = new Request();
+        $request = new Approver();
         $businessNameList = ['id', 'Approver Full Name','Approver Email'];
         $this->assertEqualsCanonicalizing($businessNameList,$request->getBusinessNames());
     }
